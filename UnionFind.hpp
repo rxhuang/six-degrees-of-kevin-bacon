a2b320fc3cf5903a96a87b722d05dd96a166a519
Alvin <wu.alvin99@gmail.com>
2017-03-12 20:46:43 -0700

    seg fault again, hehe

--- UnionFind.hpp
+++ UnionFind.hpp
@@ -1,5 +1,5 @@
-#ifndef UNIONFIND_HPP
-#define UNIONFIND_HPP
+#ifndef UNIONFIND_H
+#define UNIONFIND_H
 #include <unordered_map>
 #include <iostream>
 #include <string>
@@ -8,35 +8,39 @@
 using namespace std;
 
 class UnionFind{
-public:
+ public:
 
   unordered_map<string, pair<string, int>> actorSet;
 
   UnionFind(){}
   ~UnionFind(){}
 
+
   string find(string name){
-    if(actorSet[name].first !=name)
-      actorSet[name].first = find(actorSet[name].first);
-    else
-      return actorSet[name].first;
-}
+    if(actorSet[name].first !=name){
+      actorSet[name].first = find(actorSet[name].first);
+    }
+    else{
+      return actorSet[name].first;
+    }
+  }
 
-  void unionJoin(string name1, string name2){
-    string n1 = find(n1);
-    string n2 = find(n2);
+  void unionJoin(string name1, string name2){
+    string n1 = find(n1);
+    string n2 = find(n2);
 
-    if(n1==n2)
-      return;
-    else if(actorSet[n1].second >= actorSet[n2].second){
+    if(n1==n2){
+      return;
+    }
+    else if(actorSet[n1].second >= actorSet[n2].second){
       actorSet[n2].first = n1;
-      actorSet[n1].second = actorSet[n1].second + actorSet[n2].second;
-    }
-    else{
-      actorSet[n1].first = n2;
-      actorSet[n2].second = actorSet[n1].second + actorSet[n2].second;
-    }
-  }
+      actorSet[n1].second = actorSet[n1].second + actorSet[n2].second;
+    }
+    else{
+      actorSet[n1].first = n2;
+      actorSet[n2].second = actorSet[n1].second + actorSet[n2].second;
+    }
+  }
 
 };
 #endif
